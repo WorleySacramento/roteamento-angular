771066c8a9331d61307b8a49a751b3f869654c53
Worley Sacramento <worleymartinsns@gmail.com>
2026-09-09 16:08:13 -0300

    feat: route params 2

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Produto } from 'src/app/models/produto';
+import { ProdutoService } from 'src/app/services/produto.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  produto?: Produto;
+  notFound: boolean = false;
+
+  constructor(
+    private produtoService: ProdutoService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe({
+      next: (params) => {
+        const cod = Number(params['cod']);
+        this.produto = this.produtoService.getProduto(cod);
+        this.notFound = this.produto === undefined;
+      }
+    })
+  }
 
 }
