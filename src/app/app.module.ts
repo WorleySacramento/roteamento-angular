3f3b7899d0c2e6e7abddffc35b983b524952da8e
Worley Sacramento <worleymartinsns@gmail.com>
2026-09-09 15:00:06 -0300

    feat: first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FaqComponent } from './components/faq/faq.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaqComponent,
+    HomeComponent,
+    ProdutosComponent,
+    ProductDetailComponent,
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
