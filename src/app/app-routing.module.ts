e3d709c327015da245e22c4ec7eb2e82fd037c4a
Worley Sacramento <worleymartinsns@gmail.com>
2026-09-09 15:16:16 -0300

    feat: add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FaqComponent } from './components/faq/faq.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'produtos',
+    component: ProdutosComponent,
+  },
+  // {
+  //   path: 'not-found',
+  //   component: NotFoundComponent,
+  // },
+  {
+    path: 'faq',
+    component: FaqComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
